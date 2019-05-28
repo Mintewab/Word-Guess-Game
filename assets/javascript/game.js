11aa5c834ce5ab3a4690a08d002f3be06c29e306
Mintewab <demissiewminte123@gmail.com>
2019-05-27 20:15:25 -0700

    changed js scripts file and get rid of white space.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,309 +1,201 @@
+var letterSpans = new Array();
+var lettersTrack = new Array();
+var wordRight = "BCBCD";
+var wordWorking = "text";
+var letterTry = "P";
+var tries = 7;
+var wins = 0;
+var loses = 0;
+var lettersUsed = "";
+var ctx = 0;
+var A = new Array();
+var i = 0, i2 = 0, i7 = 0;
+var t = "X";
 
-    // declare global variables .....index of 0 to 25 = A to Z ....1=available to choose, 0=already choosen 
-    var letterSpans = new Array();
-    var lettersTrack = new Array();
-
-
-    var wordRight = "BCBCD";
-    var wordWorking = "text";
-    var letterTry = "P";
-
-    var tries = 7;
-
-    var wins = 0;
-    var loses = 0;
-    var lettersUsed = "";
-    var ctx = 0;
-
-    var A = new Array();
-
-    var i = 0, i2 = 0, i7 = 0;
-
-
-    var t = "X";
-
-
-    //FUNCTION runs when the body loads
-    function prettyUpPage() {
-
-        A = document.getElementsByTagName("div");
-
-        for (i = 0; i < A.length && 1 == 1; i += 1) {
-
-            A[i].style.cssText += "; border: 1px solid #009999; padding: 3px; margin: 3px;background:rgb(32, 26, 51, 0.6)";
-
-        }
-
-        letterSpans = document.getElementsByTagName("span");
-
-        for (i = 0; i < letterSpans.length && 1 == 1; i += 1) {
-
-            letterSpans[i].style.cssText += ";border: 1px solid #006666; padding: 3px; margin: 3px; cursor: pointer; color: #cc6600;";
-
-        }
-
-
-
-
-        document.getElementById("displayHungDude").style.cssText += "; border: 1px solid #cc6600; padding: 2px; margin: 2px;";
-
-
-        ctx = document.getElementById("displayHungDude").getContext("2d");
-
-
-
-        gameStart();
-
-
+// runs when the body loads
+function prettyUpPage() {
+    A = document.getElementsByTagName("div");
+    for (i = 0; i < A.length && 1 == 1; i += 1) {
+        A[i].style.cssText += "; border: 1px solid #009999; padding: 3px; margin: 3px;background:rgb(32, 26, 51, 0.6)";
     }
 
+    letterSpans = document.getElementsByTagName("span");
+    for (i = 0; i < letterSpans.length && 1 == 1; i += 1) {
+        letterSpans[i].style.cssText += ";border: 1px solid #006666; padding: 3px; margin: 3px; cursor: pointer; color: #cc6600;";
+    }
 
-    function gameStart() {
+    document.getElementById("displayHungDude").style.cssText += "; border: 1px solid #cc6600; padding: 2px; margin: 2px;";
+    ctx = document.getElementById("displayHungDude").getContext("2d");
+    gameStart();
+}
 
-        tries = 7;
-        lettersUsed = "";
+function gameStart() {
+    tries = 7;
+    lettersUsed = "";
+    t = "Pizza,Pepperoni,Mushrooms,Ham,Peppers,Onions,Olives,Bacon,Sausage,Garlic,Pineapple,Jalapenos,Tomatoes,Artichokes,Meatballs,Anchovies,Basil,Pepperponcini,Pesto,Cheese,Crust";
+    t = t.toUpperCase();
 
-        t = "Pizza,Pepperoni,Mushrooms,Ham,Peppers,Onions,Olives,Bacon,Sausage,Garlic,Pineapple,Jalapenos,Tomatoes,Artichokes,Meatballs,Anchovies,Basil,Pepperponcini,Pesto,Cheese,Crust";
+    //pick a random # from 0 to 20, interger
+    i = Math.floor(Math.random() * 21);
+    wordRight = t.split(",")[i];
+    wordWorking = "";
+    t = "";
 
-        t = t.toUpperCase();
-        //pick a random # from 0 to 20, interger
-        i = Math.floor(Math.random() * 21);
+    //if Onions, loop starts wordWorking to exactly 6 "_" and t to "_ _ _ _ _ _ "
+    //if Ham, the loop starts wordWorking to exactly 3 "_" and t to "_ _ _ "
+    for (i = 0; i < wordRight.length; i++) {
+        wordWorking += "_";
+        t += "_ ";
+    }
 
-        wordRight = t.split(",")[i];
+    document.getElementById("wordDisplay").innerHTML = "Your Word: " + t;
 
-        wordWorking = "";
-        t = "";
-
-        //if Onions, loop starts wordWorking to exactly 6 "_" and t to "_ _ _ _ _ _ "
-        //if Ham, the loop starts wordWorking to exactly 3 "_" and t to "_ _ _ "
-        for (i = 0; i < wordRight.length; i++) {
-
-            wordWorking += "_";
-            t += "_ ";
-
-        }
-        // needs to display in the html
-        document.getElementById("wordDisplay").innerHTML = "Your Word: " + t;
+    // letters to be choosen only one time
+    for (i = 0; i < 26; i++) {
+        lettersTrack[i] = 1;
+    }
+    redrawHungDude();
+    document.getElementById("Info").innerHTML = "You have 7 tries left";
+    document.getElementById("Info2").innerHTML = "Press any key to get started!";
+    document.getElementById("Info3").innerHTML = wins + " wins and " + loses + " loses";
+}
 
 
-        // letters to be choosen only one time
-        for (i = 0; i < 26; i++) {
+function KeyTheyPressed() {
+    t = event.key;
+    t = t.toUpperCase();
+    LetterClick(t);
+}
 
-            lettersTrack[i] = 1;
-        }
+function LetterClick(letterTry) {
+    i7 = letterTry.charCodeAt(0) - 65;
+    //when I already chose this letter, it display alert, and exit this function
+    if (lettersTrack[i7] < 1) {
+        alert("You aleady tried letter " + letterTry + " ... Try another!");
+        return;
+    }
+    lettersTrack[i7] = 0;
+
+    if (wordRight.indexOf(letterTry) < 0) {
+
+        letterSpans[i7].style.cssText += ";border: 3px solid #BB0000; color: #BB0000;";
+        tries--;
+        lettersUsed = lettersUsed + letterTry + " ";
         redrawHungDude();
-
-        document.getElementById("Info").innerHTML = "You have 7 tries left";
-
-        document.getElementById("Info2").innerHTML = "Press any key to get started!";
-
-        document.getElementById("Info3").innerHTML = wins + " wins and " + loses + " loses";
-        
+    } else {
+        letterSpans[i7].style.cssText += ";border: 3px solid #00FF00; color: #00FF00;";
     }
 
+    t = "";
 
-
-
-
-
-    // event created for Key pressed t=thekeytheypressed
-    function KeyTheyPressed() {
-
-
-        t = event.key;
-        t = t.toUpperCase();
-
-        LetterClick(t);
-
+    for (i = 0; i < wordRight.length; i++) {
+        if (wordRight.charAt(i) == letterTry) {
+            t += letterTry
+        } else { t += wordWorking.charAt(i) }
     }
 
-    function LetterClick(letterTry) {
+    wordWorking = t;
 
-
-
-        //the charCode of "A" is 65, but I want to set i7 to 0 if A was clicked
-        //the charCode of "B" is 66, but I want to set i7 to 1 if B was clicked, and so on....
-        i7 = letterTry.charCodeAt(0) - 65;
-
-        //when I already chose this letter, it display alert, and exit this function
-        if (lettersTrack[i7] < 1) {
-
-            alert("You aleady tried letter " + letterTry + " ... Try another!");
-            return;
-
-        }
-
-        //mark the selected letter as chosen so I dont repet
-        lettersTrack[i7] = 0;
-
-
-        // when I click letter ....the little box changes color :red for correct and red for wrong
-        // it tracks how many tries we have left as well
-        if (wordRight.indexOf(letterTry) < 0) {
-
-            letterSpans[i7].style.cssText += ";border: 3px solid #BB0000; color: #BB0000;";
-            tries--;
-            lettersUsed = lettersUsed + letterTry + " ";
-            redrawHungDude();
-        }
-
-        else {
-
-            letterSpans[i7].style.cssText += ";border: 3px solid #00FF00; color: #00FF00;";
-
-        }
-
-
-
-        // replaces "_" with the letter 
-        t = "";
-
-        for (i = 0; i < wordRight.length; i++) {
-
-            if (wordRight.charAt(i) == letterTry) {
-
-                t += letterTry
-
-            }
-
-            else { t += wordWorking.charAt(i) }
-
-        }
-
-        wordWorking = t;
-
-        // i2 = WordWorking.indexOf("_");
-
-        t = "";
-
-        for (i = 0; i < wordRight.length; i++) {
-
-            t += wordWorking.charAt(i);
-            t += " ";
-
-        }
-
-        document.getElementById("wordDisplay").innerHTML = "Your Word: " + t;
-
-
-        t = "You have " + tries + " tries left.";
-
-
-        //I lost
-        if (tries < 1) {
-
-            t = "You are a LOSER!!! (the word you were looking for was " + wordRight + ")";
-            loses++;
-
-        }
-
-        if (wordWorking.indexOf("_") < 0) {
-            t = "You WIN!!!";
-            wins++;
-
-        }
-
-        document.getElementById("Info").innerHTML = t;
-
-        document.getElementById("Info2").innerHTML = "You have used the following letters: " + lettersUsed;
-
-
-        if (tries < 1 || wordWorking.indexOf("_") < 0) { prettyUpPage() }
-
-        // document.getElementById("Info").innerHTML = t;
-
+    t = "";
+    for (i = 0; i < wordRight.length; i++) {
+        t += wordWorking.charAt(i);
+        t += " ";
     }
 
+    document.getElementById("wordDisplay").innerHTML = "Your Word: " + t;
+    t = "You have " + tries + " tries left.";
 
-    function redrawHungDude() {
+    if (tries < 1) {
+        t = "You are a LOSER!!! (the word you were looking for was " + wordRight + ")";
+        loses++;
+    }
 
-        //set some basic parameters
-        ctx.lineCap = "round";
-        ctx.lineJoin = "round";
+    if (wordWorking.indexOf("_") < 0) {
+        t = "You WIN!!!";
+        wins++;
+    }
+
+    document.getElementById("Info").innerHTML = t;
+    document.getElementById("Info2").innerHTML = "You have used the following letters: " + lettersUsed;
+    if (tries < 1 || wordWorking.indexOf("_") < 0) { prettyUpPage() }
+}
 
 
-        //background
-        ctx.fillStyle = "rgb(0, 153, 153)";
-        ctx.fillRect(20, 20, 360, 360);
+function redrawHungDude() {
+    //set some basic parameters
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
 
-        //gallows base
-        ctx.strokeStyle = "RGB(0,0,0)";
-        ctx.lineWidth = 16;
+    //background
+    ctx.fillStyle = "rgb(0, 153, 153)";
+    ctx.fillRect(20, 20, 360, 360);
+
+    //gallows base
+    ctx.strokeStyle = "RGB(0,0,0)";
+    ctx.lineWidth = 16;
+    ctx.beginPath();
+    ctx.moveTo(40, 360);
+    ctx.lineTo(80, 320);
+    ctx.lineTo(320, 320);
+    ctx.lineTo(360, 360);
+    ctx.stroke();
+
+    //gallows
+    ctx.beginPath();
+    ctx.moveTo(150, 320);
+    ctx.lineTo(150, 50);
+    ctx.lineTo(250, 50);
+    ctx.lineTo(250, 90);
+    ctx.stroke();
+
+    if (tries < 7) {
+        //head
+        ctx.fillStyle = "RGB(0,0,200)";
         ctx.beginPath();
-        ctx.moveTo(40, 360);
-        ctx.lineTo(80, 320);
-        ctx.lineTo(320, 320);
-        ctx.lineTo(360, 360);
-        ctx.stroke();
-
-        //gallows
-        ctx.beginPath();
-        ctx.moveTo(150, 320);
-        ctx.lineTo(150, 50);
-        ctx.lineTo(250, 50);
-        ctx.lineTo(250, 90);
-        ctx.stroke();
-
-        if (tries < 7) {
-
-            //head
-            ctx.fillStyle = "RGB(0,0,200)";
-            ctx.beginPath();
-            ctx.arc(250, 100, 30, 2 * Math.PI, 0);
-            ctx.fill();
-
-        }
-
-        if (tries < 6) {
-
-            //body
-            ctx.strokeStyle = "RGB(0,0,200)";
-            ctx.lineWidth = 10;
-            ctx.beginPath();
-            ctx.moveTo(250, 130);
-            ctx.lineTo(250, 230);
-            ctx.stroke();
-
-        }
-
-        if (tries < 5) {
-
-            //left leg
-            ctx.beginPath();
-            ctx.moveTo(200, 280);
-            ctx.lineTo(250, 230);
-            ctx.stroke();
-
-        }
-
-        if (tries < 4) {
-
-            //right leg
-            ctx.beginPath();
-            ctx.moveTo(300, 280);
-            ctx.lineTo(250, 230);
-            ctx.stroke();
-
-        }
-
-        if (tries < 3) {
-
-            //left arm
-            ctx.beginPath();
-            ctx.moveTo(200, 120);
-            ctx.lineTo(250, 170);
-            ctx.stroke();
-
-        }
-
-        if (tries < 2) {
-
-            //right arm
-            ctx.beginPath();
-            ctx.moveTo(300, 120);
-            ctx.lineTo(250, 170);
-            ctx.stroke();
-
-        }
+        ctx.arc(250, 100, 30, 2 * Math.PI, 0);
+        ctx.fill();
     }
+
+    if (tries < 6) {
+        //body
+        ctx.strokeStyle = "RGB(0,0,200)";
+        ctx.lineWidth = 10;
+        ctx.beginPath();
+        ctx.moveTo(250, 130);
+        ctx.lineTo(250, 230);
+        ctx.stroke();
+    }
+
+    if (tries < 5) {
+        //left leg
+        ctx.beginPath();
+        ctx.moveTo(200, 280);
+        ctx.lineTo(250, 230);
+        ctx.stroke();
+    }
+
+    if (tries < 4) {
+        //right leg
+        ctx.beginPath();
+        ctx.moveTo(300, 280);
+        ctx.lineTo(250, 230);
+        ctx.stroke();
+    }
+
+    if (tries < 3) {
+        //left arm
+        ctx.beginPath();
+        ctx.moveTo(200, 120);
+        ctx.lineTo(250, 170);
+        ctx.stroke();
+    }
+
+    if (tries < 2) {
+        //right arm
+        ctx.beginPath();
+        ctx.moveTo(300, 120);
+        ctx.lineTo(250, 170);
+        ctx.stroke();
+    }
+}
 
